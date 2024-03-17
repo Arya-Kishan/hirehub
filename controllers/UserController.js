a813aa5af7774e819de372ee978176c729494db1
Arya <arya12345kishan@gmail.com>
2024-03-18 00:40:36 +0530

    mongodbUrl

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,7 @@ exports.getAllUser = async (req, res) => {
         res.status(200).json(user);
     } catch (error) {
         console.log(error);
-        res.status(400).json({ 'message': 'Error in All User by id' });
+        res.status(400).json({ 'message': 'Error in All User' });
     }
 }
 
