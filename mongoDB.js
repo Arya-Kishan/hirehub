ff8d5ff03eaca61b7021ca976eb3844fc761d780
Arya <arya12345kishan@gmail.com>
2024-03-22 19:25:05 +0530

    urlChange

--- mongoDB.js
+++ mongoDB.js
@@ -1,27 +1,27 @@
 
-// const mongoose = require('mongoose')
-
-// mongoose.connect("mongodb+srv://arya1234:Arya12345mongO@my-mern-social.7sec6nu.mongodb.net/?retryWrites=true&w=majority&appName=my-mern-social")
-//     .then(() => {
-//         console.log("CONNECTED TO CLOUD MONGO DB");
-//     })
-//     .catch((err) => {
-//         console.log("ERROR IN CONNECTING MONGO DB");
-//         console.log(err);
-//     })
-
-
-
-
-
-
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/arya-social-media')
+mongoose.connect("mongodb+srv://arya1234:Arya12345mongO@my-mern-social.7sec6nu.mongodb.net/?retryWrites=true&w=majority&appName=my-mern-social")
     .then(() => {
-        console.log("CONNECTED TO MONGO DB");
+        console.log("CONNECTED TO CLOUD MONGO DB");
     })
     .catch((err) => {
         console.log("ERROR IN CONNECTING MONGO DB");
         console.log(err);
     })
+
+
+
+
+
+
+// const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb://localhost:27017/arya-social-media')
+//     .then(() => {
+//         console.log("CONNECTED TO MONGO DB");
+//     })
+//     .catch((err) => {
+//         console.log("ERROR IN CONNECTING MONGO DB");
+//         console.log(err);
+//     })
